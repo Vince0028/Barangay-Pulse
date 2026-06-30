65d2e8c0726c78f988b125ddf1c6449292155dc7
Vince0028 <Vince0028@users.noreply.github.com>
2026-06-30 21:33:06 +0800

    feat: configure android build gradle files for civilian and admin applications

--- brgy_pulse/android/app/build.gradle.kts
+++ brgy_pulse/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.brgy_pulse"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- brgy_pulse_admin/android/app/build.gradle.kts
+++ brgy_pulse_admin/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.brgy_pulse_admin"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
